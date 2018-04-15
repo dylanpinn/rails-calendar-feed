1f47a82ead545b4787095be8541f10c86d6996d7
Dylan Pinn <dylan@dylanpinn.com>
2018-04-15 13:22:44 +1000

    Fix Rubocop issues

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -1,3 +1,4 @@
+# frozen_string_literal: true
 # Be sure to restart your server when you modify this file.
 
 # Define an application-wide content security policy
@@ -23,4 +24,3 @@
 # For further information see the following documentation:
 # https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Content-Security-Policy-Report-Only
 # Rails.application.config.content_security_policy_report_only = true
-
